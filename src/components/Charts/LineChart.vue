3692dd54299bf79c21980e8858c68c3018522452
Anthony McDonald <a@awews.com>
2017-06-19 10:47:45 -0700

    add sample graph and chartjs

--- src/components/Charts/LineChart.vue
+++ src/components/Charts/LineChart.vue
@@ -21,7 +21,7 @@
   }
 </script>
 
-<style>
+<style scoped>
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -33,5 +33,17 @@
   .container {
     max-width: 800px;
     margin: 0 auto;
+  }
+  .Chart {
+    background: #212733;
+    border-radius: 15px;
+    box-shadow: 0px 2px 15px rgba(25, 25, 25, 0.27);
+    margin: 25px 0;
+  }
+  .Chart h2 {
+    margin-top: 0;
+    paddig: 15px 0;
+    color: rgba(255, 0,0, 0.5);
+    border-bottom: 1px solid #323d54;
   }
 </style>
