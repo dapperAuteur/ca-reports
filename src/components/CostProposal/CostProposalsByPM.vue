3ca152aac675249ca73a283da2333e2ba55f46c8
Anthony McDonald <a@awews.com>
2017-06-30 10:40:14 -0700

    confirm d3js is working

--- src/components/CostProposal/CostProposalsByPM.vue
+++ src/components/CostProposal/CostProposalsByPM.vue
@@ -6,6 +6,11 @@
 
         <figure class="image">
           <button
+            class="btn btn-warning"
+            @click="costProposalsByPM">
+            Group Cost Proposals By PM
+          </button>
+          <button
             v-if="!costProposalIdSort"
             class="btn btn-info"
             @click="idSort">
@@ -86,6 +91,47 @@
       },
       userIdSortReverse(){
         this.sortCostProposalByUserIdReverse();
+      },
+      costProposalsByPM(){
+        console.log(this.costProposals);
+        var expenses = [
+          {
+            "name": "jim",
+            "amount": 34,
+            "date": "11/12/2015"
+          },
+          {
+            "name": "carl",
+            "amount": 120.11,
+            "date": "11/12/2015"
+          },
+          {
+            "name": "jim",
+            "amount": 45,
+            "date": "12/01/2015"
+          },
+          {
+            "name": "stacy",
+            "amount": 12.00,
+            "date": "01/04/2016"
+          },
+          {
+            "name": "stacy",
+            "amount": 34.10,
+            "date": "01/04/2016"
+          },
+          {
+            "name": "stacy",
+            "amount": 44.80,
+            "date": "01/05/2016"
+          }
+        ];
+
+        var expensesByName = d3.nest().key(function(d) {
+          // console.log(d);
+          return d.name;
+        }).entries(expenses);
+        console.log(expensesByName);
       }
     }
   }
